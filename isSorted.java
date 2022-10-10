d98abcefe3a33ebcc7fe8bf0078ce3e7f6a924cb
Nabin Tiwari <96787565+tywarinabin@users.noreply.github.com>
2022-10-11 01:38:36 +0530

    Update isSorted.java

--- isSorted.java
+++ isSorted.java
@@ -1,12 +1,12 @@
 public class isSorted {
     public static boolean isSort(int arr[], int index) {
-        if(index == arr.length-1){
+        if(index == arr.length-1){  // This is hte base case when all condition become true then it return true.
             return true;
         }
-        if(arr[index] <arr[index+1]){
+        if(arr[index] <arr[index+1]){  // IF first element is less than second then call isSorted function for index+1 and index+2 .
             return isSort(arr, index+1);
         }
-        else
+        else  // If the first element is greater than Second than simply Return False to main.
         return false;
     }
     public static void main(String args []){
